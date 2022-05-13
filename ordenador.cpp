9d96fbf8a7b0ba613df7dca47707f5773da6fc91
hugofolloni <hugofollogua07@gmail.com>
2022-05-13 20:45:13 -0300

    :rocket: Fixing ordenador (thx @muanlartins)

--- ordenador.cpp
+++ ordenador.cpp
@@ -3,18 +3,20 @@
 
 #include <iostream>
 #include <string.h>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 class Vetor {
     private:
         int inicio;
         int fim;
-        float vetor[80];
+        int vetor[10000];
     public:
         Vetor();
         int adicionar(int item);
-        void mergeSort(int iniio, int fim);
-        void merge(int inicio, int meio, int fim);
+        int mergeSort(int inicio, int fim);
+        int merge(int inicio, int meio, int fim);
         void bubbleSort(int tamanho);
         void quickSort(int inicio, int fim);
         int particiona(int inicio, int fim);
@@ -29,6 +31,9 @@ int randomNumber(int min, int max){
 Vetor:: Vetor(){
     inicio = 0;
     fim = 0;
+    for(int i = 0; i < 10000; i++){
+        vetor[i] = 0;
+    }
 };
 
 int Vetor:: adicionar(int item){
@@ -38,46 +43,48 @@ int Vetor:: adicionar(int item){
     return item;
 };
 
-void Vetor:: mergeSort(int inicio, int fim){
+int Vetor:: mergeSort(int inicio, int fim){
+    int meio = 0;
     if(inicio < fim){
         int meio = (inicio + fim) / 2;
         mergeSort(inicio, meio);
         mergeSort(meio + 1, fim);
         merge(inicio, meio, fim);
     }
+    return 0;
 };
 
-void Vetor:: merge(int inicio, int meio, int fim){
-    int vetorTempSize = fim - inicio;
+int Vetor:: merge(int inicio, int meio, int fim){
+    int vetorTempSize = fim - inicio + 5;
     int vetorTemp[vetorTempSize];	
     int i = inicio;
     int j = meio + 1;
-    int pos = inicio;
+    int pos = 0;
 
     while(i <= meio && j <= fim){
         if(vetor[i] < vetor[j]){
             vetorTemp[pos] = vetor[i];
             i++;
-        }
-        else{
+        }else{
             vetorTemp[pos] = vetor[j];
             j++;
         }
         pos++;
     }
-    while(i <= meio){
+    while (i <= meio){
         vetorTemp[pos] = vetor[i];
         i++;
         pos++;
     }
-    while(j <= fim){
+    while (j <= fim){
         vetorTemp[pos] = vetor[j];
         j++;
         pos++;
     }
-    for(int i = inicio; i <= fim; i++){
-        vetor[i] = vetorTemp[i];
-    };
+    for(int k = 0; k < pos; k++){
+        vetor[inicio + k] = vetorTemp[k];
+    }
+    return 0;
 };
 
 void Vetor:: bubbleSort(int tamanho){
@@ -142,40 +149,81 @@ void Vetor:: printVetor(string ordenador){
     printf("]\n");
 };
 
+int findChar (int argc, char *argv[], string c_){
+    int i = 0;
+    while(i < argc){
+        if(string(argv[i]) == c_){
+            return i;
+        }
+        i++;
+    }
+    return NULL;
+}
+
 int main ( int argc, char *argv[] ){
     Vetor vetor;
     string ordenador = "";
+    
 
-    int vetorSize = atoi(argv[2]);
+    int vetorSize = atoi(argv[findChar(argc, argv, "-n") + 1]);
+    
+
+    string choose = "-b";
+    if(findChar(argc, argv, "-m") != NULL){
+        choose = "-m";
+    }
+    else if(findChar(argc, argv, "-q") != NULL){
+        choose = "-q";
+    }
+
 
     cout<<"A quantidade de numeros para serem gerados no vetor: "<<vetorSize<<endl;
     cout<<"O vetor gerado foi: [ ";
-    for(int i = 0; i < vetorSize; i++){
-        cout<<vetor.adicionar(rand() % 1000)<<" ";
+    int i = 0;
+    for(i; i < vetorSize; i++){
+        cout<<vetor.adicionar(rand() % 10000)<<" ";
     }
     cout << "]" << endl;
 
     // Aplicando Bubble Sort, ou seja, algoritmo de O(n²).
-    if(argc == 3){
+    if(choose == "-b"){
+        auto start = high_resolution_clock::now();
+       
         vetor.bubbleSort(vetorSize);
+        
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+
         vetor.printVetor("bubble-sort");
+        cout << "O tempo de execucao foi: " << duration.count() << " microsegundos." << endl;
     }
-    else if( argc  == 4 ){
-        // Caso seja escolhido -m, o algoritmo é o Merge Sort.
-        if( strcmp(argv[3], "-m") == 0){
-            vetor.mergeSort(0, vetorSize);
-            vetor.printVetor("merge-sort");
-        }
-        // Caso seja escolhido, o algoritmo é o Quick Sort.
-        else if( strcmp(argv[3], "-q") == 0){
-            vetor.quickSort(0, vetorSize);
-            vetor.printVetor("quick-sort");
-        }
-        else{
-            printf("\n Argumento invalido");
-        }
+    // Caso seja escolhido -m, o algoritmo é o Merge Sort.
+    else if(choose == "-m"){
+        auto start = high_resolution_clock::now();
+
+        vetor.mergeSort(0, vetorSize -1);
+        
+        
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+
+        vetor.printVetor("merge-sort");
+        cout << "O tempo de execucao foi: " << duration.count() << " microsegundos." << endl;
     }
-    else {
-        printf("O numero de argumentos nao e valido");
+    // Caso seja escolhido, o algoritmo é o Quick Sort.
+    else if( choose == "-q"){
+        auto start = high_resolution_clock::now();
+
+        vetor.quickSort(0, vetorSize);
+
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+
+        vetor.printVetor("quick-sort");
+        cout << "O tempo de execucao foi: " << duration.count() << " microsegundos." << endl;
     }
+    else{
+        printf("\n Argumento invalido");
+    }
+
 };
